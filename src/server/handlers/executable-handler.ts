555208caa257f0de13da1540c928f1763b738dde
Eduardo Oliveira <eduardo_y05@outlook.com>
2023-08-21 13:08:13 -0300

    fix: use cwd option on executable-handler

--- src/server/handlers/executable-handler.ts
+++ src/server/handlers/executable-handler.ts
@@ -1,10 +1,12 @@
+import path from 'path';
 import { IButtonKey } from '../../interfaces';
 
 const exec = require('child_process').execFile;
 
 export const executableHandler = (keyPressed: IButtonKey) => {
   if (!keyPressed.actionConfig.exePath) return;
-  exec(keyPressed.actionConfig.exePath);
+  const cwd = path.dirname(keyPressed.actionConfig.exePath);
+  exec(keyPressed.actionConfig.exePath, undefined, { cwd });
 };
 
 export default null;
