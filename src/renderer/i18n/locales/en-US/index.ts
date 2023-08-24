3034ea737b920e5d3d68d301edd1e1fe429f153b
Willian Rodrigues <contato@willianrod.com>
2023-08-24 00:52:56 -0300

    Update src/renderer/i18n/locales/en-US/index.ts

--- src/renderer/i18n/locales/en-US/index.ts
+++ src/renderer/i18n/locales/en-US/index.ts
@@ -1,6 +1,6 @@
 import common from './common.json';
 import createPageButton from './create-page-button.json';
-import DeleteWarn from './delete-warn.json';
+import deleteWarn from './delete-warn.json';
 import editor from './editor.json';
 import header from './header.json';
 import keys from './keys.json';
