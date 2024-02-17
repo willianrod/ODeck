577a302287e582efc16e531b8f15524a33471cc4
Sergio Fernández Celorio <sfernandez@arvo.es>
2024-02-17 17:55:39 +0100

    Spanish translation (#49)

---
+++ src/renderer/i18n/locales/es-ES/index.ts
@@ -0,0 +1,17 @@
+import common from './common.json';
+import createPageButton from './create-page-button.json';
+import deleteWarn from './delete-warn.json';
+import editor from './editor.json';
+import header from './header.json';
+import handlers from './handlers.json';
+import about from './about.json';
+
+export default {
+  about,
+  common,
+  editor,
+  header,
+  handlers,
+  'create-page-button': createPageButton,
+  'delete-warn': deleteWarn,
+};
--- src/renderer/i18n/locales/index.ts
+++ src/renderer/i18n/locales/index.ts
@@ -1,9 +1,11 @@
 import en from './en-US';
 import pt from './pt-BR';
+import es from './es-ES';
 
 const resources = {
   en,
   pt,
+  es,
 };
 
 export default resources;
